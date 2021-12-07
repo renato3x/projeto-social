709efa7d8dab24063f5fbfe9b25a7e378000ab0a
renato3x <pereirarenato21@gmail.com>
2021-12-07 20:54:57 -0300

    Implementando EJS e refatorando projeto

--- src/controllers/main.controller.js
+++ src/controllers/main.controller.js
@@ -2,6 +2,6 @@ const path = require('path')
 
 module.exports = {
   index(req, res) {
-    return res.sendFile(path.join(__dirname, '..', 'views', 'main', 'index.html'))
+    return res.render(path.join(__dirname, '..', 'views', 'main', 'index.ejs'))
   }
 }
