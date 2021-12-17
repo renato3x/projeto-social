49ee46e037ede3463f9be36a97f95b9062367c6d
JorgeRoniel <jorge.infor4g@gmail.com>
2021-12-17 14:26:47 -0300

    Criação da rota /escolas

--- src/routes/main.routes.js
+++ src/routes/main.routes.js
@@ -3,5 +3,6 @@ const router = Router()
 const mainController = require('../controllers/main.controller')
 
 router.get('/', mainController.index)
+router.get('/escolas', mainController.escolas)
 
 module.exports = router
