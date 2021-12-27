e95fbdf5c8d95feaa2adb2e48740ae69a8eeb818
JorgeRoniel <jorge.infor4g@gmail.com>
2021-12-27 18:03:22 -0300

    Criação das rotas para as páginas /sobre_nos e /projetos

--- src/routes/main.routes.js
+++ src/routes/main.routes.js
@@ -4,5 +4,7 @@ const mainController = require('../controllers/main.controller')
 
 router.get('/', mainController.index)
 router.get('/escolas', mainController.escolas)
+router.get('/projetos', mainController.projetos)
+router.get('/sobre_nos', mainController.sobre_nos)
 
 module.exports = router
