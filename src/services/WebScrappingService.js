e7d23c0eab32aa09b2d68622da495f0fdc2c5e26
renato3x <pereirarenato21@gmail.com>
2021-12-29 20:25:00 -0300

    insta scraping: evitando erro de nao conseguir os posts

--- src/services/WebScrappingService.js
+++ src/services/WebScrappingService.js
@@ -7,30 +7,48 @@ const fsExtra = require('fs-extra')
 module.exports = class WebScrappingService {
   async getInstagramPosts() {
     fsExtra.emptyDir(path.join(__dirname, '..', 'public', 'highlights', 'insta'))
-    
-    const { medias } = await ig.scrapeUserPage('seducarcarnaubal')
+    let data = undefined
 
-    const data = medias.map(({ node: post }, index) => {
-      if (index > 2) {
-        return
-      }
+    try {
+      const { medias } = await ig.scrapeUserPage('seducarcarnaubal')
 
-      const filename = `insta_highlight_${index}.jpg`
+      data = medias.map(({ node: post }, index) => {
+        if (index > 2) {
+          return
+        }
 
-      axios({
-        url: post.display_url,
-        responseType: 'stream'
-      }).then(({ data: response }) => {
-        response
-        .pipe(fs.createWriteStream(path.join(__dirname, '..', 'public', 'highlights', 'insta', filename)))
+        const filename = `insta_highlight_${index}.jpg`
+
+        axios({
+          url: post.display_url,
+          responseType: 'stream'
+        }).then(({ data: response }) => {
+          response
+          .pipe(fs.createWriteStream(path.join(__dirname, '..', 'public', 'highlights', 'insta', filename)))
+        })
+
+        return { imgSrc: `/highlights/insta/${filename}`, postLink: `https://instagram.com/p/${post.shortcode}` }
+      }).filter(x => x != undefined)
+    } catch(error) {
+      data = [
+        {
+          imgSrc: '/img/setembro_amarelo.jpg',
+          postLink: 'https://www.instagram.com/p/CTqaEkgr4h3/'
+        },
+        {
+          imgSrc: '/img/outubro_rosa.jpg',
+          postLink: 'https://www.instagram.com/p/CU6J1YJrge7/'
+        },
+        {
+          imgSrc: '/img/novembro_azul.jpg',
+          postLink: 'https://www.instagram.com/p/CVxeO60rqSQ/'
+        }
+      ]
+    } finally {
+      fs.writeFileSync(path.join(__dirname, '..', 'public', 'highlights', 'insta', '.gitkeep', ''), '', {
+        encoding: 'utf-8'
       })
-
-      return { imgSrc: `/highlights/insta/${filename}`, postLink: `https://instagram.com/p/${post.shortcode}` }
-    }).filter(x => x != undefined)
-
-    fs.writeFileSync(path.join(__dirname, '..', 'public', 'highlights', 'insta', '.gitkeep', ''), '', {
-      encoding: 'utf-8'
-    })
+    }
 
     return data
   }
